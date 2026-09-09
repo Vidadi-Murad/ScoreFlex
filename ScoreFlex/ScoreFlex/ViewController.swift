033884ef14591dabcbb46e48e81b53b4bce8cdbf
VidadiMurad <vidad0murad@gmail.com>
2026-09-09 19:15:07 +0400

    First Commit

---
+++ ScoreFlex/ScoreFlex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScoreFlex
+//
+//  Created by Vidadi Muradzada on 09.09.26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
